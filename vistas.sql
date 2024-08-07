2bbc1ae49b0a64c51afea3fd8b78d102f854b447
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2024-08-06 23:59:50 -0300

    Update vistas.sql

--- vistas.sql
+++ vistas.sql
@@ -1,3 +1,4 @@
+-- VISTA DE DETALLE DE RESERVAS --
 CREATE VIEW view_detalle_reserva AS
 SELECT 
     r.reserva_id,
@@ -21,3 +22,21 @@ JOIN
     tiporeserva tr ON r.id_tipo_res = tr.id_tipo_res
 JOIN 
     pago p ON r.reserva_id = p.reserva_id;
+
+--------------------------------------------------------------
+
+-- VISTA DE INGRESOS --
+CREATE VIEW vista_ingresos_por_hotel AS
+SELECT 
+    h.nombre AS nombre_hotel,
+    SUM(p.monto) AS ingresos_totales
+FROM 
+    hotel h
+JOIN 
+    habitacion hab ON h.hotel_id = hab.hotel_id
+JOIN 
+    reserva r ON hab.habitacion_id = r.habitacion_id
+JOIN 
+    pago p ON r.reserva_id = p.reserva_id
+GROUP BY 
+    h.nombre;
